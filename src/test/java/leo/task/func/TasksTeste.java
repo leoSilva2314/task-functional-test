921c3108395591e1173ddaaf967535f1a81e056b
Leonardo <lfpereira@viajanet.com.br>
2022-12-23 16:04:14 -0300

    versao 2

--- src/test/java/leo/task/func/TasksTeste.java
+++ src/test/java/leo/task/func/TasksTeste.java
@@ -1,5 +1,7 @@
 package leo.task.func;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -7,18 +9,25 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TasksTeste {
 	
-	public WebDriver accesarAplicacao () {
+	private Object cap;
+	private WebDriver driver;
+
+	public WebDriver accesarAplicacao () throws MalformedURLException {
 		WebDriver driver = new ChromeDriver();
+		//DesiredCapabilities cap = DesiredCapabilities.chrome();
+		//RemoteWebDriver WebDriver = new RemoteWebDriver(new URL("http://192.168.1.107:4444/wd/hub"), cap);
 		driver.navigate().to("http://localhost:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
 	
 	@Test
-	public void deveSalvarTarefaComSucesso() {
+	public void deveSalvarTarefaComSucesso() throws MalformedURLException {
 		WebDriver driver = accesarAplicacao();
 		try {
 		//clicar em add todo
@@ -44,7 +53,7 @@ public class TasksTeste {
 	}
 		
 		@Test
-		public void naodeveSalvarTarefaSemData() {
+		public void naodeveSalvarTarefaSemData() throws MalformedURLException {
 			WebDriver driver = accesarAplicacao();
 			try {
 			//clicar em add todo
@@ -67,7 +76,7 @@ public class TasksTeste {
 		}
 		
 		@Test
-		public void naoDeveSalvarTarefaComDataPassada() {
+		public void naoDeveSalvarTarefaComDataPassada() throws MalformedURLException {
 			WebDriver driver = accesarAplicacao();
 			try {
 			//clicar em add todo
